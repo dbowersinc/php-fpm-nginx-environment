cdac3bc00bf9a44058fa77c0defe9a4d72300f16
David Bowers <10davidbowers01@gmail.com>
2021-09-08 16:03:33 -0700

    Started a backup tool script

--- backup-scripts/backuptovol.sh
+++ backup-scripts/backuptovol.sh
@@ -1,2 +1,13 @@
 #! /bin/bash
 
+# create a container to hold the data volume
+# TODO pass an argument with the volume name
+
+docker run --name dbstore -v /php-fpm-nginx-environment_data-09082021 debian /bin/bash
+
+# TODO add a statement to verify everything is going along as planned.
+docker run --rm --volumes-from dbstore -v $(pwd):/backup debian tar -cvf /backup/backup.tar /php-fpm-nginx-environment_data-09082021 
+
+# TODO give an attaboy if the file is created
+# TODO take an argument for the filename
+
